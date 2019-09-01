e367f03704483ace50edab40496c33185226bf01
dmasieh <dmasieh@gmail.com>
2019-09-01 16:07:29 -0400

    Use of fake example video to demo placement of video player container and other elements' placement on page

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" class="container">
+  <div id="app">
     <!--
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
--- src/components/VidContainer.vue
+++ src/components/VidContainer.vue
@@ -1,18 +1,70 @@
 <template>
     <div id="VidContainer">
-        <div class="row">
-            <div class="col s12 m12 l7">
-                This is where the video player will go
+        <nav>
+            <div class="nav-wrapper container">
+            <a href="#!" class="brand-logo">Logo</a>
+            <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+            <ul class="right hide-on-med-and-down">
+                <li><a href="sass.html">Sass</a></li>
+                <li><a href="badges.html">Components</a></li>
+                <li><a href="collapsible.html">Javascript</a></li>
+                <li><a href="mobile.html">Mobile</a></li>
+            </ul>
             </div>
+        </nav>
 
-            <div class="col s12 m12 l5">
-                This is where the Search Bar and Other Videos Section will go
-            </div>
+        <ul class="sidenav" id="mobile-demo">
+            <li><a href="sass.html">Sass</a></li>
+            <li><a href="badges.html">Components</a></li>
+            <li><a href="collapsible.html">Javascript</a></li>
+            <li><a href="mobile.html">Mobile</a></li>
+        </ul>
+        <main>
+            <div class="row container">
+                <div class="col s12 m12 l7">
+                    <!--This is where the video player will go-->
+                    <video class="responsive-video" controls>
+                        <source src="../assets/movie.mp4" type="video/mp4">
+                    </video>
+                </div>
 
-            <div class="col s12">
-                This is where the Footer will go
+                <div class="col s12 m12 l5">
+                    <!--This is where the Search Bar and Other Videos Section will go-->
+                    <form>
+                        <div class="input-field">
+                            <input id="search" type="search" required>
+                            <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+                            <i class="material-icons">close</i>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
+        </main>
+        <footer class="page-footer">
+            <div class="container">
+                <div class="row">
+                <div class="col l6 s12">
+                    <h5 class="white-text">Footer Content</h5>
+                    <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                </div>
+                <div class="col l4 offset-l2 s12">
+                    <h5 class="white-text">Links</h5>
+                    <ul>
+                    <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                    <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                    <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                    <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                    </ul>
+                </div>
+                </div>
+            </div>
+            <div class="footer-copyright">
+                <div class="container">
+                © 2014 Copyright Text
+                <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+                </div>
+            </div>
+        </footer>
     </div>
 </template>
 
